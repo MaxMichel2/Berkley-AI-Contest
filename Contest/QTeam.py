497b64c5d37679d307fa922a3f005099457829be
Maxime <maxime_michel_uk@hotmail.com>
2019-06-09 15:56:34 +0200

    Ajout des fonction Agent Q-learning

--- Contest/QTeam.py
+++ Contest/QTeam.py
@@ -86,7 +86,6 @@ class DummyAgent(CaptureAgent):
     Your initialization code goes here, if you need any.
     '''
 
-
   def chooseAction(self, gameState):
     """
     Picks among actions randomly.
@@ -135,15 +134,150 @@ class DummyAgent(CaptureAgent):
     """
     return {'successorScore': 1.0}
 
-class ApproxQAgentStructure(CaptureAgent):
+class ApproxQAgent(CaptureAgent):
   """
   An agent should be able to change from offensive to 
   defensive depending on the information it has access 
   to. What will change is the algorithm used to choose
   actions; Q-learning or Alpha-Beta.
   """
+  # All the following function are those used in the Project 3 Approximate Q Agent
+  def __init__(self, extractor='IdentityExtractor', actionFn = None, epsilon=0.05, gamma=0.8, alpha=0.2, numTraining=0, **args):
+    self.featExtractor = util.lookup(extractor, globals())()
+    args['epsilon'] = epsilon
+    args['gamma'] = gamma
+    args['alpha'] = alpha
+    args['numTraining'] = numTraining
+    if actionFn == None:
+        actionFn = lambda state: state.getLegalActions()
+    self.actionFn = actionFn
+    self.episodesSoFar = 0
+    self.accumTrainRewards = 0.0
+    self.accumTestRewards = 0.0
+    self.numTraining = int(numTraining)
+    self.epsilon = float(epsilon)
+    self.alpha = float(alpha)
+    self.discount = float(gamma)
+    self.weights = util.Counter()
+    self.values = util.Counter()
+    
+  def final(self, state):
+    deltaReward = state.getScore() - self.lastState.getScore()
+    self.observeTransition(self.lastState, self.lastAction, state, deltaReward)
+    self.stopEpisode()
+
+    # Make sure we have this var
+    if not 'episodeStartTime' in self.__dict__:
+        self.episodeStartTime = time.time()
+    if not 'lastWindowAccumRewards' in self.__dict__:
+        self.lastWindowAccumRewards = 0.0
+    self.lastWindowAccumRewards += state.getScore()
+
+    NUM_EPS_UPDATE = 100
+    if self.episodesSoFar % NUM_EPS_UPDATE == 0:
+        print 'Reinforcement Learning Status:'
+        windowAvg = self.lastWindowAccumRewards / float(NUM_EPS_UPDATE)
+        if self.episodesSoFar <= self.numTraining:
+            trainAvg = self.accumTrainRewards / float(self.episodesSoFar)
+            print '\tCompleted %d out of %d training episodes' % (
+                    self.episodesSoFar,self.numTraining)
+            print '\tAverage Rewards over all training: %.2f' % (
+                    trainAvg)
+        else:
+            testAvg = float(self.accumTestRewards) / (self.episodesSoFar - self.numTraining)
+            print '\tCompleted %d test episodes' % (self.episodesSoFar - self.numTraining)
+            print '\tAverage Rewards over testing: %.2f' % testAvg
+        print '\tAverage Rewards for last %d episodes: %.2f'  % (
+                NUM_EPS_UPDATE,windowAvg)
+        print '\tEpisode took %.2f seconds' % (time.time() - self.episodeStartTime)
+        self.lastWindowAccumRewards = 0.0
+        self.episodeStartTime = time.time()
+
+    if self.episodesSoFar == self.numTraining:
+        msg = 'Training Done (turning off epsilon and alpha)'
+        print '%s\n%s' % (msg,'-' * len(msg))
+        
+  def observationFunction(self, state):
+    """
+        This is where we ended up after our last action.
+        The simulation should somehow ensure this is called
+    """
+    if not self.lastState is None:
+        reward = state.getScore() - self.lastState.getScore()
+        self.observeTransition(self.lastState, self.lastAction, state, reward)
+    return state
   
-  def getFeatures(self, gameState, action):
+  def getQValue(self, state, action):
+    """
+		  Should return Q(state,action) = w * featureVector
+		  where * is the dotProduct operator
+		"""
+    "*** YOUR CODE HERE ***"
+
+    return self.getWeights() * self.featExtractor.getFeatures(state, action) # Equivalent to calculating a linear formula
+  
+  def update(self, state, action, nextState, reward):
+    """
+		Should update your weights based on transition
+		"""
+    "*** YOUR CODE HERE ***"
+
+    def multiplyAll(counter, multiplier): # Function to multiply all features by the corresponding (alpha * difference)
+      multiplier = float(multiplier)
+      for key in counter.keys():
+        counter[key] *= multiplier
+
+    difference = reward + self.discount * self.getValue(nextState) - self.getQValue(state, action) # Difference formula from Approximate Q-learning
+    features = self.featExtractor.getFeatures(state, action) # Get the features of the environment
+    multiplyAll(features, (self.alpha * difference))
+    self.weights += features
+    
+  def getPolicy(self, state):
+        return self.computeActionFromQValues(state)
+
+  def getValue(self, state):
+      return self.computeValueFromQValues(state)
+    
+  def computeValueFromQValues(self, state):
+    """
+    Returns max_action Q(state,action)
+    where the max is over legal actions.  Note that if
+    there are no legal actions, which is the case at the
+    terminal state, you should return a value of 0.0.
+    """
+    "*** YOUR CODE HERE ***"
+
+    legalActions = self.getLegalActions(state) # Fetch all possible actions at the given state
+
+    if not legalActions: # No legal actions indicates we're in the terminal state
+      return 0.0
+
+    return self.getQValue(state, self.getPolicy(state)) # Gets the Q-value from the state and policy at the given state
+		
+  def computeActionFromQValues(self, state):
+		"""
+		Compute the best action to take in a state.  Note that if there
+		are no legal actions, which is the case at the terminal state,
+		you should return None.
+		"""
+		"*** YOUR CODE HERE ***"
+
+		legalActions = self.getLegalActions(state) # Fetch all possible actions at the given state
+
+		if not legalActions: # No legal actions indicates we're in the terminal state
+			return None
+
+		return max(legalActions, key=lambda action: self.getQValue(state, action)) # Get the maximum Q-value and return the corresponding action
+  
+  def getWeights(self, gameState, action):
+    """
+    Here is where the weights of the features are determined
+    and applied to the game. These weights will change as
+    the game evolves and actions are executed.
+    """
+    return self.weights
+  
+  def getFeatures(self, gameState, action): 
     """
     Here is the place where features are chose for agents.
     Approximate Q-learning has some defined features
@@ -151,11 +285,4 @@ class ApproxQAgentStructure(CaptureAgent):
     evolution.
     Alpha-Beta also has some pre-defined features and 
     weights also !!!(A vérifier)!!!
-    """
-    
-  def getWeights(self, gameState, action):
-    """
-    Here is where the weights of the features are determined
-    and applied to the game. These weights will change as
-    the game evolves and actions are executed.
     """
